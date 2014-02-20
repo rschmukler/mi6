865e91e80da95d4792aef0ceb666ac5a9e54016d
Ryan Schmukler <rschmukler@gmail.com>
2014-02-19 21:23:20 -0500

    Mid implement

--- test/spy-test.js
+++ test/spy-test.js
@@ -1,6 +1,26 @@
+var expect = require('expect.js');
+
+var Spy = require('../lib/spy.js');
+
 describe('Spy', function() {
   describe('contructor', function() {
-    it('returns a spy');
-    it('can be called without new');
+    it('returns a spy', function() {
+      var spy = new Spy();
+      expect(spy).to.be.a(Function);
+    });
+    it('can be called without new', function() {
+      var spy = Spy();
+      expect(spy).to.be.a(Function);
+    });
+  });
+
+  describe('call', function() {
+    it('sets the _fn', function() {
+      var spy = new Spy(),
+          bogus = function() { };
+
+      spy.call(bogus);
+      expect(spy._fn).to.be(bogus);
+    });
   });
 });
