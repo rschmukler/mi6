9879dce5bb357d0f62b0257c419a480b181bc15a
Ryan Schmukler <rschmukler@gmail.com>
2014-02-19 22:28:40 -0500

    Implemented first set of spy methods

--- test/spy-test.js
+++ test/spy-test.js
@@ -3,24 +3,126 @@ var expect = require('expect.js');
 var Spy = require('../lib/spy.js');
 
 describe('Spy', function() {
+  var spy;
+  beforeEach(function() {
+    spy = new Spy();
+  });
   describe('contructor', function() {
     it('returns a spy', function() {
-      var spy = new Spy();
       expect(spy).to.be.a(Function);
     });
+
     it('can be called without new', function() {
-      var spy = Spy();
+      spy = Spy();
       expect(spy).to.be.a(Function);
+    });
+
+    it('sets the original fn', function() {
+      var a = function() { };
+      var spy = new Spy(a);
+      expect(spy._original).to.be(a);
     });
   });
 
-  describe('call', function() {
-    it('sets the _fn', function() {
-      var spy = new Spy(),
-          bogus = function() { };
+  it('errors if you misconfigure a spy', function() {
+    var spy = new Spy();
+    spy.returns(1).calls(function() { return 2; });
+    expect(spy).to.throwError();
+  });
 
-      spy.call(bogus);
-      expect(spy._fn).to.be(bogus);
+  describe('#calls', function() {
+    it('sets the _fn', function() {
+      var called = false;
+      var bogus = function() { called = true; };
+      spy.calls(bogus);
+      spy();
+      expect(called).to.be(true);
+    });
+
+    it('returns the spy', function() {
+      expect(spy.calls()).to.be(spy);
+    });
+  });
+
+  describe('#callsThrough', function() {
+    it('sets the _fn to _original', function() {
+      var called = false;
+      var bogus = function() { called = true; };
+      var spy = new Spy(bogus);
+      spy.callsThrough();
+      spy();
+      expect(called).to.be(true);
+    });
+
+    it('returns the spy', function() {
+      expect(spy.callsThrough()).to.be(spy);
+    });
+  });
+
+  describe('#returns', function() {
+    it('sets _returns', function() {
+      spy.returns(1);
+      var result = spy();
+      expect(result).to.be(1);
+    });
+
+    it('returns the spy', function() {
+      expect(spy.returns()).to.be(spy);
+    });
+  });
+
+  describe('#called', function() {
+    it('returns _called', function() {
+      spy();
+      expect(spy.called()).to.be(true);
+    });
+  });
+
+  describe('#callCount', function() {
+    it('returns the number of times called', function() {
+      spy();
+      spy();
+      spy();
+      expect(spy.callCount()).to.be(3);
+    });
+  });
+
+  describe('#calledWith', function() {
+    it('returns _calledWith with no arguments', function() {
+      spy._calledWith = [1, 2, 3];
+      expect(spy.calledWith()).to.eql([1, 2, 3]);
+    });
+
+    describe('with arguments', function() {
+      it('returns false if it has not been called', function() {
+        expect(spy.calledWith(1)).to.be(false);
+      });
+
+      it('returns false if no equality with arguments', function() {
+        spy(2);
+        expect(spy.calledWith(1)).to.be(false);
+      });
+
+      it('returns true if equality with arguments', function() {
+        spy(1, 2, 3);
+        expect(spy.calledWith(1, 2, 3)).to.be(true);
+      });
+    });
+  });
+
+  describe('#reset', function() {
+    it('resets _called', function() {
+      spy();
+      spy.reset();
+      expect(spy.called()).to.be(false);
+    });
+    it('resets _calledWith', function() {
+      spy(2);
+      spy.reset();
+      expect(spy.calledWith()).to.be(undefined);
+    });
+    it('returns the spy', function() {
+      expect(spy.reset()).to.be(spy);
     });
   });
 });
